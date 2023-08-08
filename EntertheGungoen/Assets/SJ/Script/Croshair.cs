43c80077b372575a653d397a613592ab8c736ef2
urshin <136461531+urshin@users.noreply.github.com>
2023-08-08 17:22:47 +0900

    수정사항

---
+++ EntertheGungoen/Assets/SJ/Script/Croshair.cs
@@ -0,0 +1,28 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Croshair : MonoBehaviour
+{
+    public Rigidbody2D rb; //플레이어 리지드 바디
+    private Camera _camera; //메인 카메라 받기
+    // Start is called before the first frame update
+    void Start()
+    {
+        _camera = Camera.main; //카메라 받아오기.
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Vector2 mousePos = _camera.ScreenToWorldPoint(Input.mousePosition);
+        // 마우스 위치 == 크로스헤어 위치
+        Vector2 dirVec = mousePos;
+
+
+
+        gameObject.transform.position = dirVec;
+        
+    }
+}
